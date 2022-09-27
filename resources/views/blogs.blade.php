c78d76c111740e3072bff754edc4c4a3ae6280bf
gusradika <aditezo24@gmail.com>
2022-09-27 18:57:11 +0700

    Blade Templating Engine

---
+++ resources/views/blogs.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/main')
+
+@section('container')
+
+<h1>Halaman Blogs</h1>
+
+@endsection
---
+++ resources/views/home2.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/main')
+
+@section('container')
+
+<p>Halo nama saya {{ $nama }}, Saya berkuliah di {{ $universitas }}. nim saya adalah {{ $nim }}. berjurusan {{ $jurusan }}.</p>
+
+@endsection
