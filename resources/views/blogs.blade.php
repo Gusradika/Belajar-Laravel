da283e12f3f15555fe89749f67aab695d41525a0
gusradika <aditezo24@gmail.com>
2022-09-27 19:22:24 +0700

    Blogs Posts Routes

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -2,6 +2,12 @@
 
 @section('container')
 
-<h1>Halaman Blogs</h1>
+@foreach($posts as $x)
+<article class="mb-5">
+    <h2>{{ $x["judul"] }}</h2>
+    <h3>{{ $x["author"] }}</h3>
+    <p>{{ $x["body"] }}</p>
+</article>
+@endforeach
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ["title" => "Home"]);
 });
 
 Route::get('/blogs', function () {
-    return view('blogs');
+
+    $blog_posts = [
+        [
+            "judul" => "Ashiapp1",
+            "author" => "Aditya Kesuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nemo aliquam amet minima! Nostrum voluptatibus laudantium, quaerat, veniam, ex sapiente maxime ea perspiciatis eius est accusantium suscipit laboriosam esse sequi laborum libero repellat saepe blanditiis cupiditate optio asperiores quod beatae! Assumenda, soluta vel itaque quia ratione veritatis provident consequatur debitis eos? Laudantium at, harum dolor fuga consequuntur doloribus magnam necessitatibus nulla asperiores non rem sint porro libero totam ea eum amet consectetur quam officia eaque veritatis blanditiis sequi fugit mollitia?"
+        ],
+        [
+            "judul" => "Ashiap2",
+            "author" => "Aditya Kesuma",
+            "body"  => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam dolore eos perferendis, fugiat voluptas sit provident neque quia dolores rerum quisquam, culpa nemo sed, fuga animi modi repellendus minima repudiandae! Eius accusantium soluta cumque dolor nulla fugiat maxime earum."
+        ]
+    ];
+    return view('blogs', ["title" => "Blogs", "posts" => $blog_posts]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ["title" => "About"]);
 });
 
 Route::get('/home', function () {
-    return view('home');
+    return view('home', ["title" => "Home"]);
 });
 
 // Routing with Variable
