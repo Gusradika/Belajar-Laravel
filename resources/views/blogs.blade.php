8ebf2a98b661fba2ee67ebbee646ba940b5c1135
gusradika <aditezo24@gmail.com>
2022-09-27 20:04:04 +0700

    Single Post Logic

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -4,7 +4,7 @@
 
 @foreach($posts as $x)
 <article class="mb-5">
-    <h2>{{ $x["judul"] }}</h2>
+    <h2> <a href="/blogs/{{ $x['slug'] }}"> {{ $x["judul"] }} </a></h2>
     <h3>{{ $x["author"] }}</h3>
     <p>{{ $x["body"] }}</p>
 </article>
--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,49 @@ Route::get('/blogs', function () {
     $blog_posts = [
         [
             "judul" => "Ashiapp1",
+            "slug" => "ashiapp1",
             "author" => "Aditya Kesuma",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nemo aliquam amet minima! Nostrum voluptatibus laudantium, quaerat, veniam, ex sapiente maxime ea perspiciatis eius est accusantium suscipit laboriosam esse sequi laborum libero repellat saepe blanditiis cupiditate optio asperiores quod beatae! Assumenda, soluta vel itaque quia ratione veritatis provident consequatur debitis eos? Laudantium at, harum dolor fuga consequuntur doloribus magnam necessitatibus nulla asperiores non rem sint porro libero totam ea eum amet consectetur quam officia eaque veritatis blanditiis sequi fugit mollitia?"
         ],
         [
             "judul" => "Ashiap2",
+            "slug" => "Ashiap2",
             "author" => "Aditya Kesuma",
             "body"  => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam dolore eos perferendis, fugiat voluptas sit provident neque quia dolores rerum quisquam, culpa nemo sed, fuga animi modi repellendus minima repudiandae! Eius accusantium soluta cumque dolor nulla fugiat maxime earum."
         ]
     ];
     return view('blogs', ["title" => "Blogs", "posts" => $blog_posts]);
+});
+
+// Single Post
+Route::get('/blogs/{slug}', function ($slug) {
+
+    $blog_posts = [
+        [
+            "judul" => "Ashiapp1",
+            "slug" => "ashiapp1",
+            "author" => "Aditya Kesuma",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nemo aliquam amet minima! Nostrum voluptatibus laudantium, quaerat, veniam, ex sapiente maxime ea perspiciatis eius est accusantium suscipit laboriosam esse sequi laborum libero repellat saepe blanditiis cupiditate optio asperiores quod beatae! Assumenda, soluta vel itaque quia ratione veritatis provident consequatur debitis eos? Laudantium at, harum dolor fuga consequuntur doloribus magnam necessitatibus nulla asperiores non rem sint porro libero totam ea eum amet consectetur quam officia eaque veritatis blanditiis sequi fugit mollitia?"
+        ],
+        [
+            "judul" => "Ashiap2",
+            "slug" => "Ashiap2",
+            "author" => "Aditya Kesuma",
+            "body"  => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quibusdam dolore eos perferendis, fugiat voluptas sit provident neque quia dolores rerum quisquam, culpa nemo sed, fuga animi modi repellendus minima repudiandae! Eius accusantium soluta cumque dolor nulla fugiat maxime earum."
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('blog', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
 
 Route::get('/about', function () {
